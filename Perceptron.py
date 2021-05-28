e9ed8c33a00987c122a097207a49fedeafcc4cc2
martiros888 <harutunyan.martiros@mail.ru>
2021-05-28 04:34:04 +0400

    ad

--- Perceptron.py
+++ Perceptron.py
@@ -1,20 +1,24 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
+from numba import njit, prange
+import matplotlib.pyplot as plt
+
 # page 58
 class Perceptron():
-    def __init__(self,eta=0.01,n_iter=50,random_state=1):
+    def __init__(self, eta=0.01, n_iter=50, random_state=1):
         self.eta = eta
         self.n_iter = n_iter
         self.random_state = random_state
 
-    def fit(self,X,y):
+    def fit(self, X, y):
         rgen = np.random.RandomState(self.random_state)
         self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
         self.errors_ = []
-        for _ in range(self.n_iter):
+        for _ in prange(self.n_iter):
+            print(self.errors_)
             errors = 0
-            for xi, target in zip(X,y):
+            for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
                 self.w_[1:] += update * xi
                 self.w_[0] += update
@@ -22,9 +26,20 @@ class Perceptron():
             self.errors_.append(errors) 
         return self
 
-    def net_input(self,X):
-        return np.dot(X,self.w_[1:]) + self.w_[0]
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
 
-    def predict(self,X):
+    def predict(self, X):
         return np.where(self.net_input(X) >= 0.0, 1, -1)
         
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
+df = pd.read_csv(url, header=None,encoding='utf-8')
+y = df.iloc[0:100,4].values
+y = np.where(y == 'Iris-setosa',-1,1)
+X = df.iloc[0:100, [0, 2]].values
+ppn = Perceptron(eta=0.01, n_iter=10)
+ppn.fit(X, y)
+plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
+plt.xlabel('Эпохи')
+plt.ylabel('Количества обновление')
+plt.show()
